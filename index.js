380b9c4000a149f7e36d59d777cdc1764eea6000
Francesco Menzani <francesco@menzani.eu>
2018-01-03 21:11:12 +0100

    Improve responsiveness in WebKit-based browsers

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-let preview, configSample
+let loading, preview, configSample
+let isWebkit
 
 document.addEventListener("DOMContentLoaded", function() {
+	loading = document.getElementById("loading")
 	preview = document.getElementById("preview")
 	configSample = document.getElementById("configSample")
 	
+	isWebkit = 'WebkitAppearance' in document.documentElement.style
+	if (!isWebkit) {
+		loading.style.display = "none"
+	}
+	
 	hljs.initHighlightingOnLoad()
 })
+
+function hideLoading(event) {
+	if (!isWebkit) return
+	if (event && event.animationName != "movePreview") return
+	loading.style.visibility = "hidden"
+}
+
+function showLoading() {
+	if (!isWebkit) return
+	loading.style.visibility = "visible"
+}
 
 function hidePreview() {
 	preview.style.display = "none"
